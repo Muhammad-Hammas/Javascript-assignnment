1756c6a7d81639e67ea6095e50afe5b6c8a3a335
Muhammad Hammas <muhammadhammas78@gmail.com>
2022-03-25 21:31:31 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,92 @@
+
+// alert("Hello")
+
+
+// alert("Welcome to \njava Class");
+
+
+// var name = "Hammas";
+// console.log(name);
+
+
+// var age = 21;
+// alert(name + age );
+
+
+// alert("PIZZA \nPIZZ \nPIZ \nPI \nP");
+
+
+// var emailAddress = "muhammadhammas78@gmail.com";
+
+
+// document.write("G Sir javascript se b content likhna Agya");
+
+
+// var content = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// document.write(content);
+
+
+// var birthYear = 2000;
+// document.write(birthYear);
+
+
+// prompt("Your Name");
+
+
+// var firstName = "Muhammad"
+// var lastName = "Hammas"
+// var age = 21;
+
+                // 5 legal variable names
+
+// var name = 1;
+// var name1 = 2;
+// var $name = 3;
+// var fullName = 4;
+// var your_name = 5;
+
+                // 5 illegal variable names
+
+// var &age = 21;
+// var 1name = 5;
+// var my name = 6;
+// var your-section = "b";
+// var %percentage = 90%;
+
+// var num1 = 30;
+// var num2 = 20;
+// document.write(num1 + num2);
+
+
+// var num1 = 30;
+// var num2 = 20;
+// document.write(num1 - num2);
+
+
+// var num1 = 30;
+// var num2 = 20;
+// document.write(num1 * num2);
+
+
+// var num1 = 30;
+// var num2 = 20;
+// document.write(num1 / num2);
+
+
+// var num1 = 10;
+// document.write(num1)
+// var num1 = 5
+// document.write(num1)
+// var num2 = ++num1
+// document.write(num2)
+// var num3 = num2 + 7
+// document.write(num3)
+// var num4 = --num3
+// document.write(num4)
+// document.write(num4 % 3)
+
+
+var
+
+
+
