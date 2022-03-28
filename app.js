76aac3126aeb43a0f5b2fe1614f0df55e6753114
Muhammad-Hammas <100984421+Muhammad-Hammas@users.noreply.github.com>
2022-03-29 02:22:51 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,92 +1,280 @@
 
-// alert("Hello")
+            // Chapter1
+// Q NO 1 
+        //alert("Hello");
 
+// Q NO 2
+        //document.write(Hello);
 
-// alert("Welcome to \njava Class");
+// Q NO 3
+        //alert("Welcome to \njava Class");
 
+            // CHAPTER 2
+// Q NO 1
+        //var name = "Hammas";
 
-// var name = "Hammas";
-// console.log(name);
+// Q NO 2
+        //var myName = "Muhammad Hammas"
 
+// Q NO 3
+        // var title = "Hello World"
+        // alert(title);
 
-// var age = 21;
-// alert(name + age );
+// // Q NO 4
+        // var myName ="Hammas";
+        // var age = 21;
+        // var certified = "Mobile App development;
+        // alert(myName);
+        // alert(age);
+        // alert(certified);
 
+// Q NO 5
+        //alert("PIZZA \nPIZZ \nPIZ \nPI \nP");
 
-// alert("PIZZA \nPIZZ \nPIZ \nPI \nP");
+// Q NO 6
+        // var email = "muhammadhammas78@gmail.com";
+        // alert("My Email is" + " " + email);
 
+// Q NO 7
+        // var book = "A smart way to learn JavaScript";
+        // alert(book);
 
-// var emailAddress = "muhammadhammas78@gmail.com";
+// Q NO 8
+        // document.write("G Sir javascript se b content likhna Agya");
 
+// Q NO 9
+        // var content = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+        // alert(content);
+        // document.write(content);
 
-// document.write("G Sir javascript se b content likhna Agya");
+                // CHAPTER 3
+// Q NO 1
+        // var age = 21;
+        // alert("My age is " + age);
 
+// Q NO 2
+        // alert("You have visited 1 time");
+        // alert("You have visited 2 times");
+        // alert("You have visited 3 times");
+        // alert("You have visited 4 times");
+        // alert("You have visited 5 times");
 
-// var content = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
-// document.write(content);
+// Q NO 3
+        // var birthYear = 2000; 
+        // document.write(birthYear);
 
+// Q NO 4
 
-// var birthYear = 2000;
-// document.write(birthYear);
+        // var userName = prompt( "Your name name");
+        // var product = prompt("which product you want");
+        // var quantity = +prompt("Quantity");
+        // alert(userName + " has ordered " + quantity + "items of " + product);
 
+               // CHAPTER 4
+// Q NO 1
+        // var firstName = "Muhammad"
+        // var lastName = "Hammas"
+        // var age = 21;
 
-// prompt("Your Name");
-
-
-// var firstName = "Muhammad"
-// var lastName = "Hammas"
-// var age = 21;
-
+// Q NO 2 AND 3
                 // 5 legal variable names
-
-// var name = 1;
-// var name1 = 2;
-// var $name = 3;
-// var fullName = 4;
-// var your_name = 5;
-
+        // var name = 1;
+        // var name1 = 2;
+        // var $name = 3;
+        // var fullName = 4;
+        // var your_name = 5;
                 // 5 illegal variable names
+        // var &age = 21;
+        // var 1name = 5;
+        // var my name = 6;
+        // var your-section = "b";
+        // var %percentage = 90%;
 
-// var &age = 21;
-// var 1name = 5;
-// var my name = 6;
-// var your-section = "b";
-// var %percentage = 90%;
+                // CHAPTER 5
+// Q NO 1
+        // var num1 = 30;
+        // var num2 = 20;
+        // document.write(num1 + num2);
 
-// var num1 = 30;
-// var num2 = 20;
-// document.write(num1 + num2);
+// Q NO 2
+        // var num1 = 40;
+        // var num2 = 20;
+        // document.write(num1 - num2);
 
+// Q NO 2.2
+        // var num1 = 30;
+        // var num2 = 10;
+        // document.write(num1 * num2);
 
-// var num1 = 30;
-// var num2 = 20;
-// document.write(num1 - num2);
+// Q NO 2.3
+        // var num1 = 30;
+        // var num2 = 6;
+        // document.write(num1 / num2);
 
+// Q NO 3
+        // var num1 = 10;
+        // document.write(num1) br;
+        // var num1 = 5;
+        // document.write(num1) br;
+        // var num2 = ++num1;
+        // document.write(num2) br;
+        // var num3 = num2 + 7;
+        // document.write(num3) br;
+        // var num4 = --num3;
+        // document.write(num4) br;
+        // document.write(num4 % 3);
 
-// var num1 = 30;
-// var num2 = 20;
-// document.write(num1 * num2);
+                // CHAPTER 6 TO 9
+// Q NO 1
+        // var num1 = 10;
+        // var num2 = --num1;
+        // document.write(num2);
 
+        // var num3 = 20;
+        // var num4 = ++num3;
+        // document.write(num4);
 
-// var num1 = 30;
-// var num2 = 20;
-// document.write(num1 / num2);
+        // var num5 = 20;
+        // var num5 = num5++ ;
+        // document.write(num5);
 
+// Q NO 2
+        // var a = 2;
+        // var b = 1;
+        // var result = --a - --b + ++b + b--
+        //              1 - 0 + 1 + 1
+        //              = -1 Answer
 
-// var num1 = 10;
-// document.write(num1)
-// var num1 = 5
-// document.write(num1)
-// var num2 = ++num1
-// document.write(num2)
-// var num3 = num2 + 7
-// document.write(num3)
-// var num4 = --num3
-// document.write(num4)
-// document.write(num4 % 3)
+// Q NO 3
+        // var userName = "Enter Your name";
+        // alert(userName);
 
+// Q NO 4 and 5
+        // var userNum = +prompt("Enter Number", "Please Enter");
+        // var result = 5*userNum
+        // alert(result);
 
-var
+// Q NO 6
+        // var sub1 = prompt("Enter 1st Subject Name")
+        // var sub2 = prompt("Enter 2nd Subject Name")
+        // var sub3 = prompt("Enter 3rd Subject Name")
+        // var totalMarks = 100;
+        // var mark1 = +prompt("Enter obtained marks of subject1")
+        // var mark2 = +prompt("Enter obtained marks of subject2")
+        // var mark3 = +prompt("Enter obtained marks of subject3")
+        // document.write("Sub &nbsp &nbsp &nbsp" + " Total &nbsp &nbsp" + " Obt &nbsp &nbsp" + " Percentage <br/> ")
+        // document.write(sub1 + " &nbsp &nbsp &nbsp " + totalMarks+ " &nbsp &nbsp &nbsp " + mark1 + " &nbsp &nbsp &nbsp " + "%" + mark1/totalMarks*100 +"<br/>");
+        // document.write(sub2 + " &nbsp &nbsp &nbsp " + totalMarks+ " &nbsp &nbsp &nbsp " + mark2 + " &nbsp &nbsp &nbsp " + "%" + mark2/totalMarks*100 +"<br/>");
+        // document.write(sub3 + " &nbsp &nbsp &nbsp " + totalMarks+ " &nbsp &nbsp &nbsp " + mark3 + " &nbsp &nbsp &nbsp " + "%" + mark3/totalMarks*100);
+
+                    // CHAPTER 9TO12
+// Q NO 1
+//     var cityName = prompt("Enter City Name")
+//     if (cityName == "karachi" | "KARACHI" | "Karachi"){
+//         alert("WELCOME TO CITY OF LIGHTS")
+//     }else if (cityName == "lahore" | "LAHORE" | "Lahore"  ){
+//         alert("Welcome to city of peoples where they never honestly say the right destination")
+//     }else if(cityName == "Islamabad" | "islamabad" | "isl" | "ISL" ) {
+//         alert("Welcome to the Capital city of Pakistan")
+//     } else if ( cityName == "Quetta" | "quetta"){
+//         alert("Welcome to Quetta ")
+//     }else{
+//         alert("Try another city")
+//     }
+
+// Q NO 2
+        // var gender = prompt("Enter Your Gender")
+        // if(gender == "male" | "MALE" | "Male" | "Men" | "men"){
+        //     alert("Hi Sir")
+        // }
+        // else if (gender == "female" | "FEMALE" | "Female" | "women" | "WOMEN" | "Women" ){
+        //     alert("Hello Madam")
+        // }
+        // else{
+        //     alert("Are you tiktoker")
+        // }
+
+// Q NO 3
+        // var signal = prompt("Enter any color of traffic signal")
+        // if(signal == "red" | "Red" | "RED" ){
+        //     alert("Must Stop")
+        // }else if ( signal == "green" | "GREEN" | "Green" ){
+        //     alert("Move now")
+        // }else if ( signal == "yellow" | "YELLOW" | "Yellow" ){
+        //     alert("Ready to move")
+        // }else{
+        //     alert("Aisa Color hota hai Traffic Signal Me?")
+        // }
+
+// Q NO 4
+        // var fuel = prompt ("How much fuel remains in your vehicle" ," Must used litre in the end")
+        // if(fuel <= "1 litre" | "1litre" | "1ltr" | "1 ltr"){
+        //     alert("Please refill the fuel in your car")
+        // }else(fuel >= "1 litre" | "1litre" | "1ltr" | "1 ltr" ){
+        //     alert("You have enough fuel to drive")
+        // };
+
+// Q NO 6
+        // var subject1 = +prompt("Marks in English","1 to 100")
+        // var subject2 = +prompt("Marks in Math","1 to 100")
+        // var subject3 = +prompt("Marks in Urdu","1 to 100")
+        // var marksObtained = subject1 + subject2 + subject3
+        // var totalMarks = 300;
+        // var percentage = marksObtained/totalMarks*100;
+        // alert("Your Percentage is " + percentage)
+        // if(percentage >= 80 && percentage <=100 ){
+        //     alert("GRADE = A+")
+        // }else if (percentage >= 70 && percentage <=80 ){
+        //     alert("GRADE = A")
+        // }else if (percentage >= 60 && percentage <=70 ){
+        //     alert("GRADE = B")
+        // }else if (percentage >= 50 && percentage <=40 ){
+        //     alert("GRADE = C")
+        // }else if (percentage >= 40 && percentage <=30 ){
+        //     alert("GRADE = D")
+        // }else if (percentage >= 33 && percentage <=40 ){
+        //     alert("GRADE = E")
+        // }else if (percentage <=33 ){
+        //     alert("GRADE = FAIL")
+        // }else {
+        //     alert("Please give correct information")
+        // }
+
+// Q NO 7
+
+        // var number = +prompt("Guess number (1 to 10)")
+        // if (number == 9 ){
+        //     alert("Wow That's Great")
+        // }else if ( number == ++number | --number){
+        //     alert("Close enough to the correct answer")
+        // }else {
+        //     alert ("Wrong Answer")
+        // }
+
+// Q NO 10
+        // var temperature = +prompt("Enter Temperature value ")
+        // if (temperature <= 10 ){
+        //     alert("“OMG! Today’s weather is so Cool.")
+        // }
+        // else if ( temperature <= 20){
+        //     alert("Today’s Weather is cool.")
+        // }
+        // if ( temperature <= 30){
+        //     alert("The Weather today is Normal.")
+        // }
+        // if (temperature >= 40 && temperature <= 60){
+        //     alert("It is too hot outside.")
+        // }
+        // else{
+        //     alert("Venus Pe ho kia")
+        // }
+
+// Q NO 11
+// var num1 = +prompt("Enter Value")
+// var operator = +prompt("Select Operator")
+// var num2 = +prompt("Enter Value")
+// var result = (num1 + operator + num2)
+// alert(result)
 
 
 
